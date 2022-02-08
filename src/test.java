e1132a3e4d145cd8a0342ca7b9fb27df3acb59c0
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-02-08 21:49:12 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int inv =0;
+        int op = 1;
+        while(n>0){
+            int od = n%10;
+            int ip = od;
+            int id = op;
+
+            inv = inv + id * (int)Math.pow(10, ip-1);
+
+            n /=10;
+            op++;
+        }
+        System.out.println(inv);
+    }
+
+}
